5dc92c64b7e4d84d8e7483de2c921ba1495dca13
SerMax20 <67185027+SerMax20@users.noreply.github.com>
2020-06-22 06:32:55 +0000

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -13,16 +13,16 @@ URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
 
 
 class MirrorStatus:
-    STATUS_UPLOADING = "Uploading"
-    STATUS_DOWNLOADING = "Downloading"
-    STATUS_WAITING = "Queued"
-    STATUS_FAILED = "Failed. Cleaning download"
-    STATUS_CANCELLED = "Cancelled"
+    STATUS_UPLOADING = "📤Uploading"
+    STATUS_DOWNLOADING = "📥Downloading"
+    STATUS_WAITING = "🕕Queued"
+    STATUS_FAILED = "Failed. 🚮Cleaning download"
+    STATUS_CANCELLED = "❌Cancelled"
     STATUS_ARCHIVING = "Archiving"
 
 
 PROGRESS_MAX_SIZE = 100 // 8
-PROGRESS_INCOMPLETE = ['▏', '▎', '▍', '▌', '▋', '▊', '▉']
+PROGRESS_INCOMPLETE = ['⣀', '⣠', '⣤', '⣴', '⣶', '⣾', '⣿']
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
@@ -77,7 +77,7 @@ def get_progress_bar_string(status):
     p = min(max(p, 0), 100)
     cFull = p // 8
     cPart = p % 8 - 1
-    p_str = '█' * cFull
+    p_str = '⣿' * cFull
     if cPart >= 0:
         p_str += PROGRESS_INCOMPLETE[cPart]
     p_str += ' ' * (PROGRESS_MAX_SIZE - cFull)
